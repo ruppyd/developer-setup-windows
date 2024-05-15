eee57edef6cf79d0470cd1d1a8aa5d40b0a89753
Ruppy <96357414+ruppyd@users.noreply.github.com>
2024-05-15 08:24:36 -0400

    Changed Git installation method
    Per Git's documentation, the proper winget command to install Git is: winget install --id Git.Git -e --source winget

--- DevSetup.ps1
+++ DevSetup.ps1
@@ -144,7 +144,8 @@ if (-Not (Get-AppxPackage | ? {$_.PackageFamilyName -eq "Microsoft.DesktopAppIns
 }
 
 # Install Development software via Winget
-winget install -s winget Microsoft.WindowsTerminal Microsoft.Powershell Git.Git Microsoft.VisualStudioCode Microsoft.DotNet.SDK.7
+winget install --id Git.Git -e --source winget
+winget install -s winget Microsoft.WindowsTerminal Microsoft.Powershell Microsoft.VisualStudioCode Microsoft.DotNet.SDK.7
 
 # Reload PATH environment
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
