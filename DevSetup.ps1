d38e66c2a2502725589849e461b79ef819a382cd
Ruppy <96357414+ruppyd@users.noreply.github.com>
2024-05-15 08:32:21 -0400

    Update Dotnet SDK to 8

--- DevSetup.ps1
+++ DevSetup.ps1
@@ -144,7 +144,7 @@ if (-Not (Get-AppxPackage | ? {$_.PackageFamilyName -eq "Microsoft.DesktopAppIns
 }
 
 # Install Development software via Winget
-winget install -s winget Microsoft.WindowsTerminal Microsoft.Powershell Git.Git Microsoft.VisualStudioCode Microsoft.DotNet.SDK.7
+winget install -s winget Microsoft.WindowsTerminal Microsoft.Powershell Git.Git Microsoft.VisualStudioCode Microsoft.DotNet.SDK.8
 
 # Reload PATH environment
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
